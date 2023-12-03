3dcbd75d4d7792464ee932c8ed17336d1441e3bd
khalid <khalid.mohamed@moringaschool.com>
2023-12-03 07:47:09 +0300

    complete

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+# Create another instance of Person
+alice = Person()
+alice.walk()  # This should print "The person is walking."
